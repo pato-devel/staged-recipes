ef1a8a3c8941852cda039aeb0a40d81de4b696f7
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2022-04-05 13:59:52 -0700

    run_test.sh: update path darwin64ClangDPInt32Opt

--- recipes/openfoam_for_pato/run_test.sh
+++ recipes/openfoam_for_pato/run_test.sh
@@ -5,7 +5,7 @@ set -x # show the commands
 echo -e "\n### TESTING OpenFOAM ###\n"
 
 if [ "$(uname)" = "Darwin" ]; then
-    if [ ! -f $PREFIX/src/volume_openfoam_for_pato/OpenFOAM/OpenFOAM-7/platforms/darwinIntel64GccDPInt32Opt/bin/blockMesh ]; then
+    if [ ! -f $PREFIX/src/volume_openfoam_for_pato/OpenFOAM/OpenFOAM-7/platforms/darwin64ClangDPInt32Opt/bin/blockMesh ]; then
 	exit 1 # error
     fi
     cd $PREFIX/src
