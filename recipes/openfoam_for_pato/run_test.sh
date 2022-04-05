b4107302aafbdd358d91025e8f70d3c824083bbd
jeremie.meurisse <jeremie.meurisse@gmail.com>
2022-04-04 19:44:24 -0700

    updated run_test.sh: if file exists

--- recipes/openfoam_for_pato/run_test.sh
+++ recipes/openfoam_for_pato/run_test.sh
@@ -4,7 +4,7 @@ set -x # show the commands
 
 echo -e "\n### TESTING OpenFOAM ###\n"
 
-if [ ! -f $CONDA_PREFIX/src/volume_pato_releases_conda/OpenFOAM/OpenFOAM-7/platforms/linux64GccDPInt32Opt/bin/blockMesh ]; then
+if [ ! -f $CONDA_PREFIX/src/volume_openfoam_for_pato/OpenFOAM/OpenFOAM-7/platforms/linux64GccDPInt32Opt/bin/blockMesh ]; then
      exit 1 # error
 fi
 
