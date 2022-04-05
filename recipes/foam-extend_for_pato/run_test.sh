53059b99daf014c2b0d45ec6481cef168876da89
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2022-04-04 23:14:50 -0700

    run_test.sh: update PREFIX

--- recipes/foam-extend_for_pato/run_test.sh
+++ recipes/foam-extend_for_pato/run_test.sh
@@ -5,13 +5,14 @@ set -x # show the commands
 echo -e "\n### TESTING FOAM-EXTEND_FOR_OPENFOAM ###\n"
 
 if [ "$(uname)" = "Darwin" ]; then
-    if [ ! -f $CONDA_PREFIX/src/volume_foam-extend_for_openfoam/foam-extend-4.1_for_openfoam-7/lib/darwinIntel64GccDPInt32Opt/lib_extend_foam.dylib ]; then
+    if [ ! -f $PREFIX/src/volume_foam-extend_for_openfoam/foam-extend-4.1_for_openfoam-7/lib/darwinIntel64GccDPInt32Opt/lib_extend_foam.dylib ]; then
         exit 1 # error
     fi
+    cd $PREFIX/src
     # detach volume
     hdiutil detach volume
 else
-    if [ ! -f $CONDA_PREFIX/src/volume_foam-extend_for_openfoam/foam-extend-4.1_for_openfoam-7/lib/linux64GccDPInt32Opt/lib_extend_foam.so ]; then
+    if [ ! -f $PREFIX/src/volume_foam-extend_for_openfoam/foam-extend-4.1_for_openfoam-7/lib/linux64GccDPInt32Opt/lib_extend_foam.so ]; then
         exit 1 # error
     fi
 fi
