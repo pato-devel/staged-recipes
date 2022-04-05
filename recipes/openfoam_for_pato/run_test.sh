35a9822362b6c7c921fd6f5241c12d0bfbb5dbc4
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2022-04-04 23:13:13 -0700

    run_test.sh: darwin

--- recipes/openfoam_for_pato/run_test.sh
+++ recipes/openfoam_for_pato/run_test.sh
@@ -4,12 +4,15 @@ set -x # show the commands
 
 echo -e "\n### TESTING OpenFOAM ###\n"
 
-if [ ! -f $CONDA_PREFIX/src/volume_openfoam_for_pato/OpenFOAM/OpenFOAM-7/platforms/linux64GccDPInt32Opt/bin/blockMesh ]; then
-     exit 1 # error
-fi
-
 if [ "$(uname)" = "Darwin" ]; then
+    if [ ! -f $CONDA_PREFIX/src/volume_openfoam_for_pato/OpenFOAM/OpenFOAM-7/platforms/darwinIntel64GccDPInt32Opt/bin/blockMesh ]; then
+	exit 1 # error
+    fi
     cd $PREFIX/src
     # detach volume
     hdiutil detach volume
+else
+    if [ ! -f $CONDA_PREFIX/src/volume_openfoam_for_pato/OpenFOAM/OpenFOAM-7/platforms/linux64GccDPInt32Opt/bin/blockMesh ]; then
+	exit 1 # error
+    fi
 fi
