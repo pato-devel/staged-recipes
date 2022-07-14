14dfe1ab331606c03f9357ced8d3e49228808c85
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2022-07-14 12:41:13 -0700

    PATO-v3.0: update build.sh volume and link

--- recipes/pato/build.sh
+++ recipes/pato/build.sh
@@ -5,7 +5,7 @@ set -x
 echo -e "\n### INSTALLING PATO ###\n"
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
-# This will allow them to be run on environment activation.     
+# This will allow them to be run on environment activation.
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
@@ -14,13 +14,39 @@ done
 
 PATO_VERSION=PATO-v3.0
 curr_dir=${PWD}
-if [ ! -d ${CONDA_PREFIX}/src/volume_pato ]; then
-    mkdir -p ${CONDA_PREFIX}/src/volume_pato
+# Create soft links for the compilers                                                                                   
+if [ "$(uname)" = "Linux" ]; then
+    dir_gcc=$(dirname `which x86_64-conda-linux-gnu-gcc`)
+    cd $dir_gcc
+    files=`find . -name "x86_64-conda-linux-gnu-*" -type f`
+    for x in $files
+    do
+        old_name=${x#"./"}
+        new_name=${x#"./x86_64-conda-linux-gnu-"}
+        if [ ! -f $new_name ]; then
+            ln -s $old_name $new_name
+        fi
+    done
+    cd $curr_dir
 fi
-cd ${CONDA_PREFIX}/src/volume_pato
+# Create volume_pato folder
+if [ ! -d ${PREFIX}/src/volume_pato ]; then
+    mkdir -p ${PREFIX}/src/volume_pato
+fi
+# Create and attach volume_pato
+if [ "$(uname)" = "Darwin" ]; then
+    if [ ! -d $PREFIX/src/volume_pato.sparsebundle ]; then
+        cd ${PREFIX}/src
+        hdiutil create -size 32g -type SPARSEBUNDLE -fs HFSX -volname volume_pato -fsargs -s volume_pato.sparsebundle
+	hdiutil attach -mountpoint volume_pato volume_pato.sparsebundle
+    fi
+fi
+# Download PATO
+cd ${PREFIX}/src/volume_pato
 echo Download ${PATO_VERSION}
 curl http://pato.ac/wp-content/uploads/${PATO_VERSION}.tar.gz --output ${PATO_VERSION}.tar.gz
 tar xvf ${PATO_VERSION}.tar.gz
+# Compile PATO
 cd ${PATO_VERSION}
 export PATO_DIR=${PWD}
 source ${PATO_DIR}/bashrc
