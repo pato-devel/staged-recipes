280403125c26b60fe957ea978adbd4b2e6166e4f
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2022-07-14 19:08:58 -0700

    PATO-v3.0: update run_test.sh PATO runtests

--- recipes/pato/run_test.sh
+++ recipes/pato/run_test.sh
@@ -2,13 +2,20 @@
 set -e  # exit when any command fails
 set -x # show the commands
 
+curr_dir=$PWD
+echo -e "\n### TESTING PATO ###\n"
+cd $PREFIX
+# run tests
+which runtests
+runtests
 
 if [ "$(uname)" = "Darwin" ]; then
     cd $PREFIX/src
     # detach volume_openfoam_for_pato
     hdiutil detach volume_openfoam_for_pato
-    # detach volume_foam-extend_for_pato 
+    # detach volume_foam-extend_for_pato
     hdiutil detach volume_foam-extend_for_pato
     # detach volume_pato
     hdiutil detach volume_pato
 fi
+cd $curr_dir
