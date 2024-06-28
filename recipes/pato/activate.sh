f96e0ba4660b93794b944179f301668e2a4e1415
Jeremie Meurisse <pato.devel@gmail.com>
2024-06-28 09:05:27 -0700

    added codesign to activate for osx-arm64

--- recipes/pato/activate.sh
+++ recipes/pato/activate.sh
@@ -70,3 +70,27 @@ else
     fi
 fi
 
+# Change the codesign author if the runtests executable is zsh killed
+if [ "$(uname)" = "Darwin" ]; then
+    for dir_i in "$CONDA_PREFIX" "$PREFIX"
+    do
+	if [ -f $dir_i/src/volume_pato/PATO-dev/install/bin/runtests ]; then
+	    output=$($dir_i/src/volume_pato/PATO-dev/install/bin/runtests -h 2>&1)
+	    output_len=${#output}
+	    if [ ! $output_len -gt 0 ]; then
+		echo "$dir_i: codesign executables and libraries in OpenFOAM, foam-extend, and PATO."
+		of_dir=$dir_i/src/volume_openfoam_for_pato/OpenFOAM/OpenFOAM-7/platforms/darwin64ClangDPInt32Opt
+		find $of_dir/lib -type f -name "*.dylib" -exec /usr/bin/codesign -f -d -s - {} \; > /dev/null 2>&1
+		find $of_dir/bin -type f -exec /usr/bin/codesign -f -d -s - {} \; > /dev/null 2>&1
+		fe_dir=$dir_i/src/volume_foam-extend_for_pato/foam-extend-4.1_for_openfoam-7/lib/darwinArm64GccDPInt32Opt
+		find $fe_dir -type f -name "*.dylib" -exec /usr/bin/codesign -f -d -s - {} \; > /dev/null 2>&1
+		mu_dir=$dir_i/src/volume_pato/PATO-dev/src/thirdParty/mutation++/install
+		find $mu_dir/lib -type f -name "*.dylib" -exec /usr/bin/codesign -f -d -s - {} \; > /dev/null 2>&1
+		find $mu_dir/bin -type f -exec /usr/bin/codesign -f -d -s - {} \; > /dev/null 2>&1
+		pa_dir=$dir_i/src/volume_pato/PATO-dev/install
+		find $pa_dir/lib -type f -name "*.dylib" -exec /usr/bin/codesign -f -d -s - {} \; > /dev/null 2>&1
+      		find $pa_dir/bin -type f -exec /usr/bin/codesign -f -d -s - {} \; > /dev/null 2>&1
+	    fi
+	fi
+    done
+fi
